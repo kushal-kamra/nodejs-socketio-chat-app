645d93fdc2a6b7ccdd973cc7dec9ef4a62573d2b
kushal-kamra <kushalkamra@gmail.com>
2020-05-07 09:01:23 +0530

    Include socket io;2A

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
@@ -22,6 +24,6 @@ app.post('/messages', (req, res) => {
     res.sendStatus(200);
 });
 
-var server = app.listen(3000, () => {
+var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port);
 });
